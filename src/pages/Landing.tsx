e23a7a5aca06b79401cfb2490e60cc7b239e6ba6
studerdude <Wyatt.tude@gmail.com>
2025-09-01 23:07:25 -0500

    feat: skull icon and background sizing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,7 +7,7 @@ export default function Landing() {
     const nav = useNavigate();
     return(
         <div className="scene">
-            <img className="scene-img" src={scene} alt="Wizard Desk Scene" />
+            {/*<img className="scene-img" src={scene} alt="Wizard Desk Scene" />*/}
             {/* Hotspot Positions*/}
             <Hotspot left={50} top={63} width={22} height={28}
                 onClick={() => nav("/desk")} label="Open the Tome" />
