fccfc8ca2ba629432b123abad6857cbbaeaf4d0f
studerdude <Wyatt.tude@gmail.com>
2025-09-03 14:50:52 -0500

    feat: changed backgrounds

--- src/pages/Desk.tsx
+++ src/pages/Desk.tsx
@@ -1,12 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-const scene = "/assets/DeskImagePlaceholder.png"
-
 export default function Desk() {
     const nav = useNavigate();
     return (
         <div className="desk">
-            <img className="desk-img" src={scene} alt="Desk close-up" />
             <button className="back" onClick={() => nav("/")}> Back</button>
         </div>
     );
